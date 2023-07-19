6b6a7f14c44455ef2dee0d31f4887f6568f7b6b8
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2023-07-19 22:10:37 +0700

    25th

--- src/pages/admin/items/StockIn.js
+++ src/pages/admin/items/StockIn.js
@@ -1,13 +1,17 @@
 import {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import Cookies from "js-cookie";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 
 export default function StockIn() {
+
+    const navigate = useNavigate();
+    const location = useLocation();
     const axiosPrivate = useAxiosPrivate();
 
     const [items, setItems] = useState();
     const [isLoading, setIsLoading] = useState(true);
+    const [isLoadingOnPay, setIsLoadingOnPay] = useState(false);
 
     const [itemsProcessing, setItemsProcessing] = useState([]);
 
@@ -27,12 +31,13 @@ export default function StockIn() {
         }
     }
 
-    const handleOnChange = (e, item) => {
+    const handleChange = (e, item) => {
+        const {name, value} = e.target;
         const itemExist = itemsProcessing.find(i => i.id === item.id);
         setItemsProcessing(
             itemsProcessing.map(i =>
                 i.id === item.id
-                    ? {...itemExist, quantity: parseInt(e.target.value)}
+                    ? {...itemExist, [name]: parseInt(value)}
                     : i
             )
         );
@@ -59,8 +64,25 @@ export default function StockIn() {
         );
     }
 
-    const handlePayNow = () => {
+    const handlePayNow = async e => {
+        e.preventDefault();
+        setIsLoadingOnPay(true);
 
+        let isMounted = true;
+        const controller = new AbortController();
+
+        try {
+            const res = await axiosPrivate.post('/stock_in',
+                {
+                items: itemsProcessing
+            },
+                {
+                signal: controller.signal
+            });
+            isMounted && navigate(location.state?.path || "/admin/items", { replace: true });
+        } catch (err) {
+            setIsLoadingOnPay(false);
+        }
     }
 
     useEffect(() => {
@@ -195,6 +217,8 @@ export default function StockIn() {
                                                             id="cost"
                                                             className="w-16 h-7 rounded-md border-0 pl-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                                             placeholder={item.cost}
+
+                                                            onChange={e => handleChange(e, item)}
                                                         />
                                                     </div>
                                                     <div className="relative rounded-md shadow-sm">
@@ -207,6 +231,8 @@ export default function StockIn() {
                                                             id="price"
                                                             className="w-16 h-7 rounded-md border-0 pl-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                                             placeholder={item.price}
+
+                                                            onChange={e => handleChange(e, item)}
                                                         />
                                                     </div>
                                                 </div>
@@ -220,8 +246,7 @@ export default function StockIn() {
 
                                                                     onClick={() => {
                                                                         if (item.quantity > 1) handleSubtractOne(item)
-                                                                    }}
-                                                            >
+                                                                    }}>
                                                                 -
                                                             </button>
                                                         </div>
@@ -231,16 +256,14 @@ export default function StockIn() {
                                                             id="quantity"
                                                             value={item.quantity}
                                                             className="text-center w-20 h-7 px-8 text-xs bg-gray-100 rounded-full border-0 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-blue-300"
-                                                            placeholder=""
 
-                                                            onChange={e => handleOnChange(e, item)}
+                                                            onChange={e => handleChange(e, item)}
                                                         />
                                                         <div className="absolute inset-y-0 right-0 flex items-center">
                                                             <button type="button"
                                                                     className="w-7 h-7 text-xs font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 
-                                                                    onClick={() => handleAddOne(item)}
-                                                            >
+                                                                    onClick={() => handleAddOne(item)}>
                                                                 +
                                                             </button>
                                                         </div>
@@ -253,13 +276,29 @@ export default function StockIn() {
                             )
                         )}
                     </ul>
-                    <button
-                        className="w-full text-white bg-blue-700 hover:bg-blue-800 active:ring-4 active:outline-none active:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    {isLoadingOnPay
+                        ? <button disabled type="button"
+                                  className="w-full inline-flex items-center justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 dark:bg-blue-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
+                            <svg aria-hidden="true" role="status"
+                                 className="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101"
+                                 fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="#E5E7EB"/>
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentColor"/>
+                            </svg>
+                            កំពុងផ្ទុក...
+                        </button>
+                        : <button
+                            className="w-full text-white bg-blue-700 hover:bg-blue-800 active:ring-4 active:outline-none active:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 
-                        onClick={handlePayNow}
-                    >
-                        បង់ឥឡូវ
-                    </button>
+                            onClick={handlePayNow}
+                        >
+                            បង់ឥឡូវ
+                        </button>
+                    }
                 </div>
             </div>
         </>
